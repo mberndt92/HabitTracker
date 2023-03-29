031f366b766e734d775342088a31361332292f50
mberndt92 <maximilian_berndt@icloud.com>
2023-03-30 08:54:15 +0900

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Maximilian Berndt on 2023/03/30.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
